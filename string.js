5634d8b773380b2b4b1e6c843432769ac428fb57
Michael Wiktorek <wiktorekmichael@gmail.com>
2019-05-05 00:07:35 -0700

    major code changes

--- string.js
+++ string.js
@@ -1,38 +1,77 @@
+const dampingConstant = -0.0000001;
 let paused = false;
 let damping = true;
-let dragging = false;
+let draggingMouse = false;
 let timeSinceRelease = 0;
 let mousePos = {
     x: 0,
     y: 0
 };
-let granularity = 4;
-let speed = 100;
-let points = [];
+let speed = 150;
+/* --- weird stuff here --- */
+const numPoints = 700;
+const stringLength = 800;
+/* --- end weird stuff --- */
+const thumbSize = 0;
+let pixelsPerStringUnit = 1;
+let pixelsPerPoint = 1;
+const distanceBetweenPoints = stringLength / (numPoints - 1);
+// data stored in string space units
+const points = [];
+
+// TODO resize handler, needs to iterate over points or something
+
+function stringSpaceToPixelSpace({ x, y }) {
+    const pixelsPerStringUnit = window.innerWidth / stringLength;
+    return {
+        x: x * pixelsPerStringUnit,
+        y: window.innerHeight / 2 - y * pixelsPerStringUnit
+    };
+}
+
+function pixelSpaceToStringSpace({ x, y }) {
+    const stringUnitsPerPixel = stringLength / window.innerWidth;
+    return {
+        x: x * stringUnitsPerPixel,
+        y: (window.innerHeight / 2 - y) * stringUnitsPerPixel
+    };
+}
 
 function initialize() {
-    for (i = 0; i < window.innerWidth / granularity; i++) {
-        points[i] = { x: granularity * i, y: 0, dx: 0, dy: 0 };
+    for (i = 0; i < numPoints; i++) {
+        points[i] = { x: distanceBetweenPoints * i, y: 0, dy: 0 };
     }
+
+    console.log(
+        window.innerWidth,
+        stringLength,
+        window.innerWidth / (stringLength - 1)
+    );
 }
 
 function drawPoints(ctx) {
-    for (i = 0; i < points.length - 1; i++) {
-        ctx.beginPath();
-        ctx.moveTo(points[i].x, canvas.height / 2 - points[i].y);
-        ctx.lineTo(points[i + 1].x, canvas.height / 2 - points[i + 1].y);
-        ctx.closePath();
-        ctx.stroke();
+    const start = stringSpaceToPixelSpace({
+        x: points[0].x,
+        y: points[0].y
+    });
+
+    ctx.beginPath();
+    ctx.moveTo(start.x, start.y);
+    for (i = 1; i < points.length; i++) {
+        const { x, y } = stringSpaceToPixelSpace({
+            x: points[i].x,
+            y: points[i].y
+        });
+        ctx.lineTo(x, y);
     }
+    ctx.stroke();
 }
 
 function drawMenu(ctx) {
-    ctx.font = "20px Arial";
-    ctx.fillStyle = "red";
     ctx.fillText("use up and down arrows to control simulation speed", 500, 60);
     ctx.fillText("simulation speed: " + speed, 500, 80);
     ctx.fillText("hit space to pause", 500, 120);
-    if (dragging) {
+    if (draggingMouse) {
         ctx.fillText(
             "your mouse position is " + mousePos.x + " " + mousePos.y,
             500,
@@ -48,87 +87,102 @@ function drawMenu(ctx) {
     }
 }
 
-function renderString(ctx) {
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawMenu(ctx);
-    ctx.strokeStyle = "red";
+function dragString() {
+    const { x, y } = pixelSpaceToStringSpace({ x: mousePos.x, y: mousePos.y });
+    for (i = 1; i < points.length; i++) {
+        points[i].dy = 0;
+        const current = points[i];
 
-    if (dragging) {
-        for (i = 0; i < points.length; i++) {
-            points[i].dy = 0;
-            points[i].ay = 0;
-
-            if (i < mousePos.x / granularity) {
-                //left side
-                const distanceY =
-                    ((mousePos.y - canvas.height / 2) / mousePos.x) *
-                        (i * granularity) +
-                    canvas.height / 2;
-                points[i].y = canvas.height / 2 - distanceY;
-            } else {
-                //right side
-                const distanceY =
-                    -(
-                        (mousePos.y - canvas.height / 2) /
-                        (canvas.width - mousePos.x)
-                    ) *
-                        (i * granularity - canvas.width) +
-                    canvas.height / 2;
-                points[i].y = canvas.height / 2 - distanceY;
+        if (current.x < x - thumbSize / 2) {
+            //left side
+            const slope = y / x;
+            points[i].y = slope * current.x;
+        } else if (current.x > x + thumbSize / 2) {
+            //right side
+            const slope = y / (stringLength - x);
+            const value = slope * (stringLength - current.x);
+            points[i].y = value;
+            if (i === points.length - 1) {
+                points[i].y = 0;
             }
-        }
-    } else {
-        for (k = 0; k < speed; k++) {
-            for (i = 1; i < points.length - 1; i++) {
-                points[i].ay =
-                    (points[i + 1].y - points[i].y) / granularity -
-                    (points[i].y - points[i - 1].y) / granularity;
-                if (damping) {
-                    points[i].dy =
-                        (points[i].dy + points[i].ay) *
-                        Math.exp(-0.0000001 * timeSinceRelease);
-                } else {
-                    points[i].dy = points[i].dy + points[i].ay;
-                }
-            }
-            for (i = 1; i < points.length - 1; i++) {
-                points[i].y = points[i].y + points[i].dy;
-            }
-            timeSinceRelease += 1;
+        } else {
+            points[i].y = y;
         }
     }
+}
+
+function dampingMultiplier(t) {
+    return damping ? Math.exp(dampingConstant * t) : 1;
+}
+
+function iterateVibration() {
+    for (k = 0; k < speed; k++) {
+        for (i = 1; i < points.length - 1; i++) {
+            const next = points[i + 1];
+            const prev = points[i - 1];
+            const current = points[i];
+            const yAcceleration =
+                (next.y - current.y) / distanceBetweenPoints -
+                (current.y - prev.y) / distanceBetweenPoints;
+            points[i].dy =
+                (current.dy + yAcceleration) *
+                dampingMultiplier(timeSinceRelease);
+        }
+        for (i = 1; i < points.length - 1; i++) {
+            points[i].y = points[i].y + points[i].dy;
+        }
+        timeSinceRelease += 1;
+    }
+}
+
+function physics() {
+    if (draggingMouse) {
+        dragString();
+    } else {
+        iterateVibration();
+    }
+}
+
+function render(ctx) {
+    drawMenu(ctx);
     drawPoints(ctx);
 }
 
-function render() {
+function tickAndRequestNextTick() {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "red";
+    ctx.strokeStyle = "red";
+
     if (paused) {
-        ctx.clearRect(700, 110, 45, 15);
         ctx.fillText("PAUSED", 700, 120);
+        render(ctx);
     } else {
-        renderString(ctx);
-        requestAnimationFrame(render);
+        physics();
+        render(ctx);
+        requestAnimationFrame(tickAndRequestNextTick);
     }
 }
 
 window.addEventListener("mousedown", function(e) {
     mousePos.x = e.clientX;
     mousePos.y = e.clientY;
-    dragging = true;
+    draggingMouse = true;
 });
 
 window.addEventListener("mousemove", function(e) {
-    if (dragging) {
+    if (draggingMouse) {
         mousePos.x = e.clientX;
         mousePos.y = e.clientY;
     }
 });
 
 window.addEventListener("mouseup", function() {
-    dragging = false;
+    draggingMouse = false;
     timeSinceRelease = 0;
 });
 
@@ -138,7 +192,7 @@ window.addEventListener("keydown", function(event) {
         const wasPaused = paused;
         paused = !paused;
         if (wasPaused) {
-            render();
+            tickAndRequestNextTick();
         }
     }
     // up arrow - speedup
@@ -163,9 +217,8 @@ window.addEventListener("keydown", function(event) {
 });
 
 function start() {
-    console.log("Started");
     initialize();
-    render();
+    tickAndRequestNextTick();
 }
 
 window.addEventListener("load", start);
